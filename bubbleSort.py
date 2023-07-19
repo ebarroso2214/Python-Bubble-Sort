7fcf70f603b82ef92382aa589abb33c769bdf811
ebarroso2214 <ebarroso2214@gmail.com>
2023-07-19 19:37:36 -0400

    finished bubble sort

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,1 +1,13 @@
 # write your bubble sort algorithm below
+
+def bubble_sort(list):
+    for i in range(len(list) - 1):
+        for num in range(len(list) -1):
+            if list[num] > list[num + 1]:
+                list[num], list[num + 1] = list[num + 1], list[num]
+    return list
+                
+sample_list = [6,5,2,1,7]
+print(f"Unsorted list: {sample_list}")
+bubble_sort(sample_list)
+print(f"Sorted list: {sample_list}")
